ed0b51e45d50b38b5d4c26c98e1f66a9e147163b
Michael Volling <mvollingjr@gmail.com>
2020-07-07 19:45:03 -0500

    Version 1.1.0 - Updated for Foundry 0.6.5

--- scripts/backgroundVolume.mjs
+++ scripts/backgroundVolume.mjs
@@ -3,7 +3,7 @@ import {createSceneSlider} from "./settings.mjs"
 import updateBackgroundVolume from "./volume.mjs";
 
 // Version
-const VERSION = "v1.0.7";
+const VERSION = "v1.1.0";
 
 // Target for end users
 const RELEASE = {
