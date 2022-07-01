e09d8e892d2c678aa9e8dba40186ac43ed623549
Michael Volling Jr <mvollingjr@gmail.com>
2022-07-01 17:49:55 -0500

    Changed version to 1.5.0

--- scripts/backgroundVolume.mjs
+++ scripts/backgroundVolume.mjs
@@ -3,7 +3,7 @@ import {createSceneSlider} from "./settings.mjs"
 import updateBackgroundVolume from "./volume.mjs";
 
 // Version
-const VERSION = "v1.4.2";
+const VERSION = "v1.5.0";
 
 // Target for end users
 const RELEASE = {
