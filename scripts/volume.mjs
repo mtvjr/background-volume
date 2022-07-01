e6e6666e8f1c8f2e5a3ce414acdb57b87eec7da4
Michael Volling Jr <mvollingjr@gmail.com>
2022-07-01 17:38:25 -0500

    Added V10 capable updateVolume function

--- scripts/volume.mjs
+++ scripts/volume.mjs
@@ -2,16 +2,42 @@ import Logger from "./common/logger.mjs"
 import {getVolume} from "./settings.mjs"
 
 /**
+ * Sets the current audible volume of the current scene.
+ * @note This function only works on Foundry V9 and earlier. For later versions, use _V10
+ * @param {float} newVolume - The new volume of the scene within [0-1]
+ */
+function updateVolume_V9(newVolume) {
+    if (canvas.ready && canvas.background.isVideo) {
+        canvas.background.bgSource.volume = newVolume;
+    }
+}
+
+/**
+ * Sets the current audible volume of the current scene.
+ * @note This function only works on Foundry V10 and later. For earlier versions, use _V9
+ * @param {float} newVolume - The new volume of the scene within [0-1]
+ */
+function updateVolume_V10(newVolume) {
+    if (canvas.ready) {
+        for ( const mesh of canvas.primary.videoMeshes ) {
+            mesh.sourceElement.volume = newVolume;
+        }
+    }
+}
+
+/**
  * Updates the background volume of the active scene
  */
 export default function updateBackgroundVolume() {
-    if (canvas.ready && canvas.background.isVideo) {
-        const ambient = game.settings.get("core", "globalAmbientVolume");
-        const background = getVolume(game.scenes.viewed);
+    const ambient = game.settings.get("core", "globalAmbientVolume");
+    const background = getVolume(game.scenes.viewed);
 
-        const newVolume = ambient * background;
+    const newVolume = ambient * background;
 
-        Logger.log(Logger.High, `Setting volume to ${newVolume}.`);
-        canvas.background.bgSource.volume = newVolume;
+    Logger.log(Logger.High, `Setting volume to ${newVolume}.`);
+    if (game.release.isNewer("9.269")) {
+        updateVolume_V10(newVolume);
+    } else {
+        updateVolume_V9(newVolume);
     }
 }
